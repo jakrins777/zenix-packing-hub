919f08a4bb535d1d64e4bf21b70e669000edad54
rayzera <rayzera000@gmail.com>
2026-05-28 08:43:33 +0000

    fix db con

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// ถ้าคุณใช้ Vite ให้ใช้ import.meta.env
-// ถ้าใช้ Create React App ให้ใช้ process.env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
